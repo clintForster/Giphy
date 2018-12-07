e607b66d1dd915ca43517671ce0c9ee456ae6961
clintforster <clinte.forster@gmail.com>
2018-12-06 19:34:14 -0800

    finished

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,12 @@
 
 var topics = ["Weird", "Interesting", "Cool", "Funny", "Awesome", "Waffles"];
 
+
 function rebuildButtons() {
     for (var i = 0; i < topics.length; i++) {
         var newBtn = $("<button>").text(topics[i]);
         $("#buttonsDiv").append(newBtn);
+        newBtn.addClass("button");
         newBtn.attr("value", topics[i]);
     }
 }
@@ -18,28 +20,56 @@ function checkArray(newSearchButton) {
     return false;
 }
 
-// Is recalling the same method everytime to create the buttons considered bad practice?
-rebuildButtons();
-
-$("#submit").on("click", function () {
-    var newSearchButton = $("#search").val();
-
-
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + newSearchButton + "&api_key=oMhVIYpS6S2rsB7Xp9J6ZdqwQnO578fq&limit=10";
+function whenPressed(queryURL) {
 
     $.ajax({
         url: queryURL,
         method: 'GET'
     }).then(function (response) {
-        for (var i = 0; i < topics.length; i++) {
+        console.log(response);
+        for (var i = 0; i < response.data.length; i++) {
+        var imgDiv = $("<div>");
+        var img = $("<img>");
+        $(img).attr("src", response.data[i].images.original_still.url);
+        $(img).attr("width", "700px");
+        $(img).attr("text-align", "center");
+        $(img).attr("data-state", "still");
+        $(img).attr("data-still", response.data[i].images.original_still.url);
+        $(img).attr("data-animate", response.data[i].images.original.url);
+        $(img).addClass("gif");
 
-            console.log(response)
-            // newSearchButton.attr("src", topics[i].)
+        $(imgDiv).attr("id", "text");
+        $(imgDiv).text("Rating: "+ response.data[i].rating);
+        $(imgDiv).prepend(img);
+        $("#row1").prepend(imgDiv);
         }
+        $(".gif").on("click", function() {
+            
+            var state = $(this).attr("data-state");
+            
+            if (state === "still") {
+              $(this).attr("src", $(this).attr("data-animate"));
+              $(this).attr("data-state", "animate");
+            } else {
+              $(this).attr("src", $(this).attr("data-still"));
+              $(this).attr("data-state", "still");
+            }
+          });
     });
+}
+// Is recalling the same method everytime to create the buttons considered bad practice?
+rebuildButtons();
 
+$(document).on("click", ".button", function () {
+    event.preventDefault();
+    var newSearchButton = $("#search").val();
+    console.log(this);
 
+    if (this.value !== "submit") {
+        newSearchButton = this.value
+    }
 
+    console.log(newSearchButton);
     if (checkArray(newSearchButton) === true || newSearchButton === "") {
         // Is this considered bad practice as well?
         $("#buttonsDiv").empty();
@@ -50,7 +80,7 @@ $("#submit").on("click", function () {
         $("#buttonsDiv").empty();
         rebuildButtons();
     }
-
-    event.preventDefault();
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + newSearchButton + "&api_key=oMhVIYpS6S2rsB7Xp9J6ZdqwQnO578fq&limit=10";
+    whenPressed(queryURL);
 
 });
